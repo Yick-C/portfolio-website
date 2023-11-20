2aa6a51fec0055dacb50fa71bb37ebcd4f1f224d
Christina <yickind@hotmail.co.uk>
2023-11-20 22:17:23 +0000

    Added styling for responsive website

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,16 +7,10 @@ import contact from "../../assets/contact.png";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <img src={logo} alt="Logo" className="logo" />
-      <div className="menu">
-        <Link activeClass="active" to='about' spy={true} smooth={true} offset={-100} duration={500} className="menuItem">Home</Link>
-        <Link activeClass="active" to='skills' spy={true} smooth={true} offset={-100} duration={500} className="menuItem">Skills</Link>
-        <Link activeClass="active" to='projects' spy={true} smooth={true} offset={-100} duration={500} className="menuItem">Projects</Link>
-        <Link activeClass="active" to='contact' spy={true} smooth={true} offset={-100} duration={500} className="menuItem">Contact</Link>
-      </div>
-      <button className="menuBtn">
-        <img src={contact} alt="contact" className="menuBtnImg" /> Contact Me
-      </button>
+        <Link activeClass="active" to='about' spy={true} smooth={true} offset={-100} duration={500} className="menuItem">Home <i class="fa-solid fa-house"></i> </Link> |
+        <Link activeClass="active" to='skills' spy={true} smooth={true} offset={-100} duration={500} className="menuItem">Skills <i class="fa-solid fa-star"></i></Link> |
+        <Link activeClass="active" to='projects' spy={true} smooth={true} offset={-100} duration={500} className="menuItem">Projects <i class="fa-solid fa-code"></i></Link> |
+        <Link activeClass="active" to='contact' spy={true} smooth={true} offset={-100} duration={500} className="menuItem">Contact <i class="fa-solid fa-at"></i></Link>
     </nav>
   );
 };
