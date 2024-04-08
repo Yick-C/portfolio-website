31c6f2e6b88e01441275d0345ba8f1c849d909e1
Christina <yickind@hotmail.co.uk>
2024-04-08 23:03:34 +0100

    Added mobile nav and styling for skills

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
-$(document).on("scroll", function() {
-    var pageTop = $(document).scrollTop();
-    var pageBottom = pageTop + $(window).height();
-    var tags = $(".tag");
-  
-    for (var i = 0; i < tags.length; i++) {
-      var tag = tags[i];
-      if ($(tag).position().top < pageBottom) {
-        $(tag).addClass("visible");
-      } else {
-        $(tag).removeClass("visible");
-      }
-    }
-  });
 
 /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
-let selectHeader = select('#header')
-if (selectHeader) {
-  const headerScrolled = () => {
-    if (window.scrollY > 100) {
-      selectHeader.classList.add('header-scrolled')
-    } else {
-      selectHeader.classList.remove('header-scrolled')
-    }
-  }
-  window.addEventListener('load', headerScrolled)
-  onscroll(document, headerScrolled)
-}
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $("#header");
+    $nav.toggleClass('header-scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
 
 function openNavbar(menu) {
   console.log("CLICKED");
