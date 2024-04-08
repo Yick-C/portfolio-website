b4cd78100b402d83814c3ed772e62571d66f3325
Christina <yickind@hotmail.co.uk>
2024-04-08 13:42:20 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# portfolio-website
+# CC Portfolio
+
+## Resources
+Placeholder pictures - https://picsum.photos/
+
+Images - https://unsplash.com/
+
+Colour themes - https://colorhunt.co/
+
+Fonts - https://fonts.google.com/
+
+SVG icon - https://www.svgrepo.com/
---
+++ index.js
@@ -0,0 +1,35 @@
+$(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+  
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+      } else {
+        $(tag).removeClass("visible");
+      }
+    }
+  });
+
+/**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+let selectHeader = select('#header')
+if (selectHeader) {
+  const headerScrolled = () => {
+    if (window.scrollY > 100) {
+      selectHeader.classList.add('header-scrolled')
+    } else {
+      selectHeader.classList.remove('header-scrolled')
+    }
+  }
+  window.addEventListener('load', headerScrolled)
+  onscroll(document, headerScrolled)
+}
+
+function openNavbar(menu) {
+  console.log("CLICKED");
+  menu.classList.toggle("open");
+}
